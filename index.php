acf8ea81e23e4c8b79715f2725a147649cbc5ea0
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2024-01-15 10:01:17 +0100

    fix

--- index.php
+++ index.php
@@ -6,11 +6,13 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <p>
         <?php 
@@ -19,9 +21,9 @@
     </p>
     <form action="./script.php" method="GET">
         <input type="hidden" name="testo" value="<?php echo $paragrafoDiTesto?>">
-        <input type="hidden" name="lunghezzaTesto" value="<?php echo $lunghezzaTesto ?>">
         <input type="text" name="parola">
         <input type="submit" value="Invia">
     </form>
 </body>
+
 </html>
--- script.php
+++ script.php
@@ -1,9 +1,8 @@
 <?php 
     $parola = $_GET["parola"];
     $testo = $_GET["testo"];
-    $lunghezzaTesto = $_GET["lunghezzaTesto"];
-    
     $newText = str_replace($parola, "***", $testo);
+    $lunghezzaTesto = strlen($newText);
 
     echo "<strong>Testo modificato:</strong> $newText <br/> <strong>Lunghezza:</strong> $lunghezzaTesto"
 ?>
